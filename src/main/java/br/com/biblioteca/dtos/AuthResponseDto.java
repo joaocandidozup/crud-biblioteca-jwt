05d1a971139661c7f201173e2358507498862e61
joaocandidozup <joao.candido@zup.com>
2025-02-24 15:53:29 -0300

    feature: Create authentication route for users

---
+++ src/main/java/br/com/biblioteca/dtos/AuthResponseDto.java
@@ -0,0 +1,10 @@
+package br.com.biblioteca.dtos;
+
+
+public class AuthResponseDto {
+    private String accessToken;
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
