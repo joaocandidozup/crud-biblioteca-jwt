309c2c2df3bfa569eecb074d7d5ae5f1afa60183
joaocandidozup <joao.candido@zup.com>
2025-02-24 16:04:21 -0300

    feat: add role-based access control

--- src/main/java/br/com/biblioteca/controllers/LivroController.java
+++ src/main/java/br/com/biblioteca/controllers/LivroController.java
@@ -4,6 +4,7 @@ import br.com.biblioteca.dtos.LivroRequestDTO;
 import br.com.biblioteca.dtos.LivroResponseDTO;
 import br.com.biblioteca.services.LivroService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,20 +19,26 @@ public class LivroController {
         this.livroService = livroService;
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping
     public ResponseEntity<List<LivroResponseDTO>> listarLivros() {
         return ResponseEntity.ok(livroService.listarLivros());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<LivroResponseDTO> cradastrarLivro (@RequestBody LivroRequestDTO requestDTO){
 
         return ResponseEntity.ok(livroService.cadastraLivro(requestDTO));
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<LivroResponseDTO> atualizarLivros(@PathVariable Long id, @RequestBody LivroRequestDTO livroDTO) {
         return ResponseEntity.ok(livroService.atualizarLivros(id, livroDTO));
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         livroService.deletar(id);
